b5d1617ef6427044dc25bd59af329151ae92ee58
WDong <wudong7182@cvte.com>
2020-03-01 16:27:05 +0800

    wudong first commit

---
+++ day1/test3.py
@@ -0,0 +1,12 @@
+number1 = 1
+number2 = 2
+unit1 = '人'
+unit2 = '眼'
+line1 = '我编程累'
+line2 = '是bug相随'
+sentence1 = '碎掉的节操满地堆'
+sentence2 = '我只求今日能早归'
+result_1 = str(number1)+str(unit1)+str(line1)+str(sentence1)
+result_2 = str(number2)+str(unit2)+str(line2)+str(sentence2)
+print("第一句话：", result_1)
+print("第二句话：", result_2)
---
+++ day1/test4.py
@@ -0,0 +1,6 @@
+slogan = '脸黑怪我咯'
+number = '7.8'
+unit = '张'
+sentence = '蓝票一个SSR都没有'
+result = str(slogan)+str(int(float(number)))+str(unit)+str(sentence)
+print(result)
